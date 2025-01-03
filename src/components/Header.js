9d12d86e7a7762c65b2381aa491f5f67e14944b5
Anil Kumar <aniljangra27@gmail.com>
2025-01-03 07:13:21 +0530

    improvements on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -32,54 +32,90 @@ const socials = [
   },
 ];
 
-const Header = () => {
-  const handleClick = (anchor) => () => {
-    const id = `${anchor}-section`;
-    const element = document.getElementById(id);
-    if (element) {
-      element.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-      });
-    }
-  };
-
-  return (
-    <Box
-      position="fixed"
-      top={0}
-      left={0}
-      right={0}
-      translateY={0}
-      transitionProperty="transform"
-      transitionDuration=".3s"
-      transitionTimingFunction="ease-in-out"
-      backgroundColor="#18181b"
-    >
-      <Box color="white" maxWidth="1280px" margin="0 auto">
-        <HStack
-          px={16}
-          py={4}
-          justifyContent="space-between"
-          alignItems="center"
-        >
-          <nav>
-          {socials.map((social, index) => (
-            <a key={index} href={social.url} target="_blank" rel="noopener noreferrer" style={{ margin: '0 10px' }}>
-              <FontAwesomeIcon icon={social.icon} size="lg" />
-            </a>
-            ))}
-          </nav>
-          <nav>
-            <HStack spacing={8}>
-              {/* Add links to Projects and Contact me section */}
-              <a onClick={handleClick("projects")}>Projects</a> 
-              <a onClick={handleClick("contactme")}>Contact me</a>
-            </HStack>
-          </nav>
-        </HStack>
-      </Box>
-    </Box>
-  );
-};
-export default Header;
+const Header = () => { 
+  const headerRef = useRef(null); 
+  
+  useEffect(() => { 
+    let prevScrollPos = window.scrollY; 
+  
+    const handleScroll = () => { 
+      const currentScrollPos = window.scrollY; 
+      const headerElement = headerRef.current; 
+      if (!headerElement) { 
+        return; 
+      } 
+      if (prevScrollPos > currentScrollPos) { 
+        headerElement.style.transform = "translateY(0)"; 
+      } else { 
+        headerElement.style.transform = "translateY(-200px)"; 
+      } 
+      prevScrollPos = currentScrollPos; 
+    } 
+    window.addEventListener('scroll', handleScroll) 
+  
+    return () => { 
+      window.removeEventListener('scroll', handleScroll) 
+    } 
+  }, []); 
+  
+  const handleClick = (anchor) => () => { 
+    const id = `${anchor}-section`; 
+    const element = document.getElementById(id); 
+    if (element) { 
+      element.scrollIntoView({ 
+        behavior: "smooth", 
+        block: "start", 
+      }); 
+    } 
+  }; 
+  return ( 
+    <Box 
+      position="fixed" 
+      top={0} 
+      left={0} 
+      right={0} 
+      translateY={0} 
+      transitionProperty="transform" 
+      transitionDuration=".3s" 
+      transitionTimingFunction="ease-in-out" 
+      backgroundColor="#18181b" 
+      ref={headerRef} 
+    > 
+      <Box color="white" maxWidth="1280px" margin="0 auto"> 
+        <HStack 
+          px={16} 
+          py={4} 
+          justifyContent="space-between" 
+          alignItems="center" 
+        > 
+          <nav> 
+            <HStack spacing={8}> 
+              {socials.map(({ icon, url }) => ( 
+                <a 
+                  key={url} 
+                  href={url} 
+                  target="_blank" 
+                  rel="noopener noreferrer" 
+                > 
+                  <FontAwesomeIcon icon={icon} size="2x" key={url} /> 
+                </a> 
+              ))} 
+            </HStack> 
+          </nav> 
+          <nav> 
+            <HStack spacing={8}> 
+              <a href="#projects" onClick={handleClick("projects")}> 
+                Projects 
+              </a> 
+              <a href="#contactme" onClick={handleClick("contactme")}> 
+                Contact Me 
+              </a> 
+            </HStack> 
+          </nav> 
+        </HStack> 
+      </Box> 
+    </Box> 
+  ); 
+ }; 
+ 
+ export default Header; 
