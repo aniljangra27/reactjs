5659fc382e9cfba4e8526137f6ad18e3a1c97c25
Anil Kumar <aniljangra27@gmail.com>
2025-02-04 14:51:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { ChakraProvider } from "@chakra-ui/react";
 import Header from "./components/Header"; 
 import LandingSection from "./components/LandingSection"; 
 import ProjectsSection from "./components/ProjectsSection"; 
-// import ContactMeSection from "./components/ContactMeSection"; 
 import Footer from "./components/Footer"; 
 import { AlertProvider } from "./context/alertContext"; 
 import Alert from "./components/Alert"; 
@@ -15,7 +14,6 @@ function App() {
          <Header /> 
          <LandingSection /> 
          <ProjectsSection /> 
-         // <ContactMeSection /> 
          <Footer /> 
          <Alert /> 
        </main> 
